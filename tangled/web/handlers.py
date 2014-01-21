TangledWeb/tangled.web

commit 4e7b35dc207fb12b7b17b1692a08c031afb37948

Fix static URL generation so it works with multi-segment prefixes

tangled/web/handlers.py
@@ -94,17 +94,11 @@ def request_finished_handler(app, request, _):
 
 
 def static_files(app, request, next_handler):
-    if app.has_any('static_directory'):
-        segments = tuple(request.path_info.lstrip('/').split('/'))
-        prefix = ()
-        for segment in segments:
-            prefix += (segment,)
-            directory_app = app.get('static_directory', prefix, None)
-            if directory_app:
-                request.is_static = True
-                for _ in prefix:
-                    request.path_info_pop()
-                return directory_app(request)
+    prefix, rel_path = app._find_static_directory(request.path_info)
+    if prefix is not None:
+        request.is_static = True
+        directory_app = app.get('static_directory', prefix)
+        return directory_app(app.make_blank_request('/'.join(rel_path)))
     return next_handler(app, request)
 
 

tangled/web/app.py
@@ -479,6 +479,38 @@ class Application(Registry):
             directory = DirectoryApp(directory, index_page=index_page)
         self.register('static_directory', directory, prefix)
 
+    def _find_static_directory(self, path):
+        """Find static directory for ``path``.
+
+        This attempts to find a registered static directory
+        corresponding to ``path``. If there is such a directory, the
+        prefix and the remaining segments are both returned as a tuple
+        of segments; if there isn't, ``(None, None)`` is returned.
+
+        E.g., for the path /static/images/icon.png, the following
+        tuple of tuples will be returned (assuming a static directory
+        was mounted with the prefix 'static')::
+
+            (('static'), ('images', 'icon.png'))
+
+        The prefix tuple can be used to find the registered static
+        directory::
+
+            app.get('static_directory', prefix)
+
+        The prefix and remaining segments can be used to generate
+        URLs.
+
+        """
+        if self.has_any('static_directory'):
+            prefix = ()
+            segments = tuple(path.lstrip('/').split('/'))
+            for segment in segments:
+                prefix += (segment,)
+                if self.contains('static_directory', prefix):
+                    return prefix, segments[len(prefix):]
+        return None, None
+
     # Non-configuration methods
 
     def notify_subscribers(self, event_type, *event_args, **event_kwargs):

tangled/web/request.py
@@ -251,18 +251,12 @@ class Request(ARequest, BaseRequest):
         '/static/images/logo.png'. SCRIPT_NAME will be prepended by
         :meth:`make_url`.
 
-        The first segment in the path ('static' in the example above)
-        will be matched against static directories registered via
-        :meth:`.app.Application:mount_static_directory`.
-
         """
-        prefix, *rel_path = path.lstrip('/').split('/')
-        try:
-            # TODO: Doesn't work for multi-segment prefixes
-            directory = self.app.get('static_directory', prefix)
-        except KeyError:
+        prefix, rel_path = self.app._find_static_directory(path)
+        if prefix is None:
             raise ValueError(
-                'Unregistered static directory: {}'.format(prefix))
+                "Can't generate static URL for {}".format(path))
+        directory = self.app.get('static_directory', prefix)
         if isinstance(directory, str):
             # E.g., http://assets.example.com/static/images/logo.png
             url = posixpath.join(directory, *rel_path)
@@ -270,7 +264,6 @@ class Request(ARequest, BaseRequest):
                 url += self.make_query_string(query)
         else:
             # E.g., /static/images/logo.png
-            path = posixpath.join(prefix, *rel_path)
             url = self.make_url(path, query, **kwargs)
         return url
 

commit a0b4d8de02aeabc217f5821d1547b57422a1ddb4

Fix static file handler bug introduced in 4e7b35d
When calling into the DirectoryApp, we need to pass a request with a
proper environ, not a blank request.

tangled/web/handlers.py
@@ -97,8 +97,11 @@ def static_files(app, request, next_handler):
     prefix, rel_path = app._find_static_directory(request.path_info)
     if prefix is not None:
         request.is_static = True
+        environ = request.environ.copy()
+        environ['PATH_INFO'] = '/' + '/'.join(rel_path)
+        static_request = app.make_request(environ)
         directory_app = app.get('static_directory', prefix)
-        return directory_app(app.make_blank_request('/'.join(rel_path)))
+        return directory_app(static_request)
     return next_handler(app, request)
 
 