96106c5883ad79f1fd9ab65dadb4d3fce2ad2a15
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-01-27 12:40:48 -0800

    Amend previous

--- tangled/web/exc.py
+++ tangled/web/exc.py
@@ -59,7 +59,7 @@ def get_exc_log_message(app, request, exc):
             message += 'Request failed hard\n' + message
     else:
         try:
-            resource_config = format_dict(request.resource_config)
+            resource_config = format_dict(request.resource_config.__dict__)
         except Exception as exc:
             resource_config = str(exc)
         message = EXC_LOG_MESSAGE_TEMPLATE.format(
