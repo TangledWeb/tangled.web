f7824af47bcb95a21019eaa7f7b1b99716e94cd7
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-02-05 15:53:32 -0800

    Prepare release 0.1a1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tangled.web',
-    version='0.1.dev0',
+    version='0.1a1',
     description='RESTful Web Framework',
     long_description=open('README.rst').read(),
     packages=find_packages(),
