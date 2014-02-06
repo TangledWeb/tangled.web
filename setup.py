18b985ba7f678696954f39d1f1725d9622d47a4a
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-02-05 16:14:04 -0800

    Back to development: 0.1a3.dev0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tangled.web',
-    version='0.1a2',
+    version='0.1a3.dev0',
     description='RESTful Web Framework',
     long_description=open('README.rst').read(),
     url='http://tangledframework.org/',
