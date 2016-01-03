f7a011196d00ff7014b11a0ab6a0d46f47f88d0e
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2016-01-03 15:19:23 -0800

    Configure app via include rather than factory in basic scaffold
    The factory approach shouldn't be recommended anywhere--it's more for
    highly advanced use cases and not for typical app config.

--- tangled/web/scaffolds/basic/__package__/__init__.py
+++ tangled/web/scaffolds/basic/__package__/__init__.py
@@ -1,9 +1,5 @@
-from tangled.web import Application
-
-
-def make_app(settings):
+def include(app):
     """Configure ${package_name}."""
-    app = Application(settings)
     app.mount_resource('home', '.resources:Hello', '/', methods='GET')
     app.mount_resource('hello', '.resources:Hello', '/<name>', methods='GET')
     return app
