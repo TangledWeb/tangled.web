b771f47ce2d74499be2f0d677cca7f3bb160998b
Wyatt Baldwin <self@wyattbaldwin.com>
2017-12-09 17:38:40 -0800

    Change version scheme from 0.1aN to 1.0aN

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, PEP420PackageFinder
 
 setup(
     name='tangled.web',
-    version='0.1a11.dev0',
+    version='1.0a11.dev0',
     description='RESTful Web Framework',
     long_description=open('README.rst').read(),
     url='http://tangledframework.org/',
