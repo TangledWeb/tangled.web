4bad2105b1fb5446ab6519c49a343f9c10ee5a4d
Wyatt Baldwin <self@wyattbaldwin.com>
2017-12-09 17:39:59 -0800

    Fix CORS handler
    Amends 413d6a06c88d597566dfc2241156e72b6c1ca730

--- tangled/web/cors.py
+++ tangled/web/cors.py
@@ -5,52 +5,51 @@ log = logging.getLogger(__name__)
 
 
 def cors_handler(app, request, next_handler, ):
-    """Handle CORS pre-flight OPTIONS requests.
+    """Handle CORS.
 
-    If the request method is OPTIONS *and* the resource does not handle
-    CORS pre-flight requests *and* permissive CORS is enabled, this will
-    return a response that allows requests from any origin for any
-    method and content type.
+    If permissive CORS is not enabled, this does nothing.
 
-    When the request method is something other than OPTIONS, this has no
-    effect.
+    If the resource handles CORS itself by adding access control headers
+    to the response, this does nothing.
 
-    When the request method is OPTIONS and the resource handles CORS
-    pre-flight requests, the resource is responsible for setting the
-    appropriate CORS headers.
+    Otherwise...
 
-    If permissive CORS is not enabled, this will return a response with
-    no access control headers, which will effectively disallow the
-    original request.
+    When the request method is OPTIONS, this will return a response that
+    allows requests from the request's origin for any method and content
+    type.
+
+    When the request method is something other than OPTIONS, this will
+    add a header to the response that allows the request.
 
     .. note:: This is mainly intended for use in development since it is
         INSECURE.
 
     """
-    skip = (
-        # Request can't be a CORS request if its method isn't OPTIONS
-        (request.method != 'OPTIONS') or
-        # Not a CORS request since there are no CORS headers
-        (not any(h.startswith('Access-Control') for h in request.headers))
-    )
-
     response = next_handler(app, request)
-
-    if skip:
-        return response
-
     permissive = app.get_setting('tangled.app.cors.permissive', False)
 
     if permissive:
         if not app.debug:
             log.warning('Permissive CORS is enabled; this is INSECURE')
-        # Only add the permissive access control headers if the resource
-        # doesn't add them itself.
-        if not any(h.startswith('Access-Control') for h in response.headers):
-            response.headers.update({
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': '*',
-                'Access-Control-Allow-Headers': 'Content-Type',
-            })
+
+        origin = request.headers.get('Origin')
+
+        if origin and not has_cors_headers(response):
+            if request.method == 'OPTIONS':
+                if has_cors_headers(request):
+                    response.headers.update({
+                        'Access-Control-Allow-Origin': origin,
+                        'Access-Control-Allow-Methods': '*',
+                        'Access-Control-Allow-Headers': 'Content-Type',
+                    })
+            else:
+                response.headers.update({
+                    'Access-Control-Allow-Origin': origin,
+                })
 
     return response
+
+
+def has_cors_headers(obj):
+    headers = obj.headers
+    return any(name.startswith('Access-Control') for name in headers)
