d0212aea443d6adb8e0a1435f53066973c765782
Wyatt Baldwin <self@wyattbaldwin.com>
2017-12-10 17:10:02 -0800

    Upgrade tangled 1.0a10 => 1.0a11

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ setup(
     packages=PEP420PackageFinder.find(include=['tangled*']),
     include_package_data=True,
     install_requires=[
-        'tangled>=1.0a10',
+        'tangled>=1.0a11',
         'MarkupSafe>=0.23',
         'WebOb>=1.5.1',
     ],
     extras_require={
         'dev': [
-            'tangled[dev]>=1.0a10',
+            'tangled[dev]>=1.0a11',
         ],
     },
     entry_points="""
